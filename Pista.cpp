9fb492305491c72398e1a7526b66c0fc17584355
KenyStev <kjestevez.0795@gmail.com>
2015-06-12 21:03:04 -0600

    off_set_x

--- Pista.cpp
+++ Pista.cpp
@@ -6,7 +6,8 @@ Pista::Pista(RosalilaGraphics *paint, Receiver* receiver)
     miLista1 =new ListImage(paint);
     miLista2 =new ListImage(paint);
     this->receiver = receiver;
-    off_set=0;
+    off_set_y=0;
+    off_set_x=0;
     background = paint->getTexture(assets_directory+"background.png");
     this->painter = paint;
     bu = paint->getTexture(assets_directory+"BACK_UP.png");
@@ -32,17 +33,25 @@ void Pista::init(){
 
 void Pista::logica(){
         if(receiver->isKeyDown(SDL_SCANCODE_UP))
-            off_set=10.5;
+            off_set_y=10.5;
         else if(receiver->isKeyDown(SDL_SCANCODE_DOWN))
-            off_set=-10.5;
+            off_set_y=-10.5;
         else
-            off_set=0;
+            off_set_y=0;
+
+        if(receiver->isKeyDown(SDL_SCANCODE_LEFT))
+            off_set_x=10.5;
+        else if(receiver->isKeyDown(SDL_SCANCODE_RIGHT))
+            off_set_x=-10.5;
+        else
+            off_set_x=0;
+
         back_button->update(receiver->getMouse_X(),receiver->getMouse_Y(),receiver->isLeftClickDown());
 }
 void Pista:: draw(){
      painter->draw2DImage(background,background->getWidth(),background->getHeight(),0,0,1,0,false,0,0,Color(255,255,255,255),0,0,false);
-     miLista1->draw(off_set);
-     miLista2->draw(off_set);
+     miLista1->draw(off_set_x,off_set_y);
+     miLista2->draw(off_set_x,off_set_y);
      back_button->draw(receiver->getMouse_X(),receiver->getMouse_Y(),receiver->isLeftClickDown());
 }
 Pista::~Pista()
